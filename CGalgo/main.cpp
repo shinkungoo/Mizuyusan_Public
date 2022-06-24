b9fe4e9730368ae5e27d63d11d736549b5b268e9
Mizuyusan <88479555+Mizuyusan@users.noreply.github.com>
2022-06-24 15:49:54 +0800

    opengl learning

--- CGalgo/main.cpp
+++ CGalgo/main.cpp
@@ -1,6 +1,27 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    #ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    #endif
+
+    GLFWwindow * windows = glfwCreateWindow(800, 600, "CGalgo", nullptr, nullptr);
+    if(windows == nullptr){
+        std::cout << "Fail to create window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(windows);
+
+
+
     return 0;
 }
