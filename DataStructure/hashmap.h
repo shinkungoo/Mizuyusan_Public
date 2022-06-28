04709cbb14f2afbb29031f8a425a12599e60ff18
Mizuyusan <88479555+Mizuyusan@users.noreply.github.com>
2022-06-29 00:24:15 +0800

    a new project about data structure

---
+++ DataStructure/hashmap.h
@@ -0,0 +1,8 @@
+//
+// Created by Shizumu on 2022/6/29.
+//
+
+#ifndef DATASTRUCTURE_HASHMAP_H
+#define DATASTRUCTURE_HASHMAP_H
+
+#endif //DATASTRUCTURE_HASHMAP_H
---
+++ DataStructure/shared_stack.h
@@ -0,0 +1,8 @@
+//
+// Created by Shizumu on 2022/6/29.
+//
+
+#ifndef DATASTRUCTURE_SHARED_STACK_H
+#define DATASTRUCTURE_SHARED_STACK_H
+
+#endif //DATASTRUCTURE_SHARED_STACK_H
