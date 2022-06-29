8976997fd961a65f44f80aed0c65496243a0e813
Mizuyusan <88479555+Mizuyusan@users.noreply.github.com>
2022-06-29 17:22:22 +0800

    要分割出setPixel方法以便于后面绘画图案

---
+++ CGalgo/utility_graphics.h
@@ -0,0 +1,10 @@
+//
+// Created by Shizumu on 2022/6/29.
+//
+
+#ifndef CGALGO_UTILITY_GRAPHICS_H
+#define CGALGO_UTILITY_GRAPHICS_H
+void Init();
+void setPixel(int x, int y);
+
+#endif //CGALGO_UTILITY_GRAPHICS_H
