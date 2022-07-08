bf1f46667f155c3c488bfc2ae0f51795df8028c1
Mizuyusan <873892608@qq.com>
2022-07-08 09:46:45 +0800

    openglLearning project initialization

---
+++ CGalgo/cmake-build-debug/CMakeFiles/CGalgo.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for CGalgo.
+# This may be replaced when dependencies are built.
---
+++ openglLearning/cmake-build-debug/CMakeFiles/openglLearning.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/openglLearning.dir/glad_WIN32.c.obj"
+  "CMakeFiles/openglLearning.dir/main.cpp.obj"
+  "libopenglLearning.dll.a"
+  "openglLearning.exe"
+  "openglLearning.exe.manifest"
+  "openglLearning.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/openglLearning.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
